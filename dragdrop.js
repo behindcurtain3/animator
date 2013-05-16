9f023978b62c72582efd5f09e08271a8e3f31488
Justin <behindcurtain3@gmail.com>
2013-05-16 01:22:52 -0700

    Added event listeners to save animation properties when they are changed

--- dragdrop.js
+++ dragdrop.js
@@ -4,7 +4,13 @@ $(document).ready(function() {
 	var img = new Image();
 	var animating = 0;
 	var animationFrame = 0;
+	var frameDelay;
 
+	function clearPreview() {
+		clearInterval(animating);
+		animating = 0;
+	}
+	
 	$("#animations").change(function() {
 		// Called when a new animation is selected from the list
 		if(currentFile == null)		
@@ -40,7 +46,9 @@ $(document).ready(function() {
 		ctx.clearRect(0, 0, canvas.width, canvas.height);
 		ctx.drawImage(img , 0, 0, img.width, img.height, 0, 0, img.width, img.height);
 		
-		clearInterval(animating);
+		frameDelay = anim.framedelay.value;
+		
+		clearPreview();
 	});
 
 	$("#animation-frames").change(function() {
@@ -57,7 +65,7 @@ $(document).ready(function() {
 		
 		ctx.strokeRect(frame.x, frame.y, frame.width, frame.height);
 		
-		clearInterval(animating);
+		clearPreview();
 	});
 	
 	// Setup default img
@@ -101,7 +109,7 @@ $(document).ready(function() {
 
 		reader.readAsText(file); 
 		
-		clearInterval(animating);
+		clearPreview();
 	}
 	
 	
@@ -110,13 +118,9 @@ $(document).ready(function() {
 			return;
 			
 		if(animating != 0) {
-			clearInterval(animating);
-			animating = 0;
+			clearPreview();
 			return;
 		}
-		
-		e.preventDefault();
-		clearInterval(animating);
 		
 		animationFrame = 0;		
 		animating = setInterval(function() {
@@ -127,19 +131,13 @@ $(document).ready(function() {
 					animationFrame = 0;
 				else
 				{
-					clearInterval(animating);
+					clearPreview();
 					return;
 				}
 			}
 				
 			var frame = anim.frames[animationFrame];
-			console.log(frame);
-			
-			//$('#animation-area').css('width', frame.width + 'px');
-			//$('#animation-area').css('height', frame.height + 'px');			
-			
 			animationFrame++;
-			
 			var canvas = $("#display").get(0);
 			var ctx = canvas.getContext('2d');
 			
@@ -150,73 +148,80 @@ $(document).ready(function() {
 			canvas = $("#animation-area").get(0);
 			ctx = canvas.getContext('2d');
 			ctx.clearRect(0, 0, canvas.width, canvas.height);
-			ctx.drawImage(img , frame.x, frame.y, frame.width, frame.height, 0, 0, frame.width, frame.height);
 			
-		}, $("#animation-framedelay").val());
-		/*
-		
-		clearInterval(animating);
-		framesX = $('#frames-X').val();
-		framesY = $('#frames-Y').val();
-		frameWidth = $('#dropbox').width() / framesX;
-		frameHeight = $('#dropbox').height() / framesY;
-		
-		$('#animation-area').css('width', frameWidth + 'px');
-		$('#animation-area').css('height', frameHeight + 'px');
-		$('#animation-area').css('background-image', 'url(' + img.src + ')');
-		
-		bpx = 0;
-		bpy = 0;
-		
-		frames = JSON.parse('{"frames":[' + $('#frames').val() +']}').frames;
-		
-		count = 0;
-		
-		if( frames.length > 2 ) {
-		
-			animating = setInterval(function(){
-				
-				if( count++ >= frames.length ) count = 0;
-				
-				bpx = -(frames[count] % framesX) * frameWidth;
-				bpy = -((frames[count] / framesX)|0) * frameHeight;
-
-				$('#animation-area').css('background-position', bpx + 'px ' + bpy + 'px' );
-			},$('#framedelay').val());
-		} else {
-		
-			if( frames.length == 2 ) {
-				startFrame = frames[0];
-				endFrame = frames[1];
-			} else if( frames.length < 2 ) {
-				startFrame = 0;
-				endFrame = framesX * framesY - 1;
-			} 
-
-			count = startFrame;
-			bpx = -(count % framesX) * frameWidth;
-			bpy = -((count / framesX)|0) * frameHeight;	
+			// Have to do this otherwise it cuts off the right/bottom pixels
+			var width = parseInt(frame.width) + 1;
+			var height = parseInt(frame.height) + 1;
 			
-			$('#animation-area').css('background-position', bpx + 'px ' + bpy + 'px' );
+			ctx.drawImage(img, frame.x, frame.y, width, height, 0, 0, width, height);
 			
-			animating = setInterval(function(){
-				
-				if( count++ >= endFrame ) {
-					count = startFrame;
-				};
-				
-				bpx = -(count % framesX) * frameWidth;
-				bpy = -((count / framesX)|0) * frameHeight;				
-
-				$('#animation-area').css('background-position', bpx + 'px ' + bpy + 'px' );
-			},$('#interval').val());
-
-		} 
-		*/
+		}, frameDelay);		
 	});
-});
-
-(function() {
+	
+	// Animation property update methods
+	$("#animation-spritesheet").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+			
+		selectedAnimation.spritesheet.value = $("#animation-spritesheet").val();		
+	});
+	
+	$("#animation-state").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+		
+		selectedAnimation.state.value = $("#animation-state").val();	
+		
+		var index = $("#animations").val();
+		$("#animations option[value='" + index + "']").replaceWith('<option value=' + index + '>' + selectedAnimation.state.value + ' [' + selectedAnimation.group.value + ']</option>');
+		$("#animations").val(index);
+	});
+	
+	$("#animation-group").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+			
+		selectedAnimation.group.value = $("#animation-group").val();		
+		
+		var index = $("#animations").val();
+		$("#animations option[value='" + index + "']").replaceWith('<option value=' + index + '>' + selectedAnimation.state.value + ' [' + selectedAnimation.group.value + ']</option>');
+		$("#animations").val(index);
+	});
+	
+	$("#animation-layer").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+			
+		selectedAnimation.layer.value = $("#animation-layer").val();		
+	});
+	
+	$("#animation-origin").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+			
+		selectedAnimation.origin.value = $("#animation-origin").val();		
+	});
+	
+	$("#animation-framedelay").focusout(function(e) {		
+		if(currentFile == null || selectedAnimation == null)
+			return;
+			
+		selectedAnimation.framedelay.value = $("#animation-framedelay").val();		
+		frameDelay = selectedAnimation.framedelay.value;
+	});
+	
+	$("#animation-loop").click(function(e) {
+		if(currentFile == null || selectedAnimation == null)
+			return;
+		
+		if($("#animation-loop").is(':checked')) {
+			selectedAnimation.loop = true;
+		} else {
+			selectedAnimation.loop = false;
+		}
+	});
+	
+	// Change the spritesheet
 	var dropbox = document.getElementById('drop-area'); 
 
 	dropbox.addEventListener('dragenter', stopDefault, false); 
@@ -245,20 +250,17 @@ $(document).ready(function() {
 		if (!file.type.match(imageType)) { 
 			return; 
 		} 
-
-
+		
 		var reader = new FileReader(); 
-
 
 		reader.onerror = function(e) { 
 			alert('Error code: ' + e.target.error); 
 		}; 
 
-
 		reader.onload = (function(aFile) { 
 			return function(evt) { 
 				var ctx = $('#display').get(0).getContext('2d');
-				var img = new Image();   // Create new img element  
+				img = new Image();   // Create new img element  
 				img.onload = function(){  
 					$('#display').attr('width', img.width);
 					$('#display').attr('height', img.height);
@@ -270,6 +272,10 @@ $(document).ready(function() {
 		})(file); 
 
 		reader.readAsDataURL(file); 
-	} 
+	}
+});
+
+(function() {
+	 
 	
 })();
