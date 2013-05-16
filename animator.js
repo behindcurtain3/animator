07b4cc05e1cd64c572a70789ef9ab034a79bd28b
Justin <behindcurtain3@gmail.com>
2013-05-15 17:41:04 -0700

    Large modifications, added ability to drag and drop a .anim file

--- animator.js
+++ animator.js
@@ -1,3 +1,64 @@
+function Animations(canvasElement) {
+	// Canvas
+	this.canvasElement = canvasElement;
+	this.ctx = this.canvasElement.getContext("2d");
+	
+	// Animations
+	this.animations = [];	 
+}
+
+Animations.prototype = {
+	load: function(file) {
+		parser = new DOMParser();
+		xmlDoc = parser.parseFromString(file,"text/xml");
+		
+		var animationNodes = xmlDoc.getElementsByTagName("Animation");		
+		this.animations = new Array();
+		$("#animations").html("");
+		
+		for(var i = 0; i < animationNodes.length; i++) {
+			var anim = new Animation();
+			anim.load(animationNodes[i]);
+			this.animations[i] = anim;			
+			
+			$("#animations").append('<option value=\"' + i + '\">' + anim.state.value + ' [' + anim.group.value + ']</option>');
+		}
+		
+		console.log(this.animations);
+	}
+}
+
+function Animation() {
+	this.state = "";
+	this.group = "";
+	this.loop = false;
+	this.spritesheet = "";
+	this.layer = 0;
+	this.frameDelay = 100;
+	this.origin = "";	
+	this.frames = [];
+}
+
+Animation.prototype = {
+	load: function(xmlNode) {
+		this.state = xmlNode.getAttributeNode("State");
+		this.group = xmlNode.getAttributeNode("Group");
+		this.loop = xmlNode.getAttributeNode("Loop");
+		this.spritesheet = xmlNode.getAttributeNode("SpriteSheet");
+		this.layer = xmlNode.getAttributeNode("Layer");
+		this.frameDelay = xmlNode.getAttributeNode("FrameDelay");
+		this.origin = xmlNode.getAttributeNode("Origin");
+	}
+}
+
+function Frame() {
+	this.x = 0;
+	this.y = 0;
+	this.width = 32;
+	this.height = 32;
+}
+/*
+
 (function() {
 
 	var animating = 0;
@@ -64,4 +125,4 @@
 		
 	});
 
-})();
+})(); */
--- dragdrop.js
+++ dragdrop.js
@@ -1,3 +1,53 @@
+$(document).ready(function() {
+	var ctx = $('#display').get(0).getContext('2d');
+	
+	var img = new Image();   // Create new img element  
+    img.onload = function(){  
+		$('#display').attr('width', img.width);
+		$('#display').attr('height', img.height);
+		ctx.drawImage(img , 0, 0, img.width, img.height, 0, 0, img.width, img.height);
+    };  
+    img.src = 'default.png'; // Set source path  	
+	
+	// Setup XML file drop listeners
+	$("#xml-drop-area").bind('dragenter dragover dragleave', stopDefault); 
+	$("#xml-drop-area").bind('drop', onXmlDrop);
+	
+	function stopDefault(e) { 
+		e.stopPropagation(); 
+		e.preventDefault(); 
+	} 
+	
+	function onXmlDrop(e) {
+		console.log(e.originalEvent);
+	
+		e.stopPropagation(); 
+		e.preventDefault(); 
+
+		var readFileSize = 0; 
+		var files = e.originalEvent.dataTransfer.files;
+
+		var file = files[0]; 
+		readFileSize += file.fileSize; 		
+		
+		var reader = new FileReader(); 
+		
+		reader.onerror = function(e) { 
+			alert('Error code: ' + e.target.error); 
+		}; 
+		
+		reader.onload = (function(aFile) { 
+			return function(evt) { 
+				var canvas = $('#display').get(0);
+				var animations = new Animations(canvas);
+				animations.load(evt.target.result);
+			} 
+		})(file); 
+
+		reader.readAsText(file); 
+	}
+});
+
 (function() {
 	var dropbox = document.getElementById('drop-area'); 
 
@@ -17,10 +67,7 @@
 		e.preventDefault(); 
 
 		var readFileSize = 0; 
-		var files = e.dataTransfer.files; 
-		
-		dropbox.style.width = "inherit";
-		dropbox.style.height = "inherit";
+		var files = e.dataTransfer.files;
 
 		file = files[0]; 
 		readFileSize += file.fileSize; 
@@ -42,7 +89,15 @@
 
 		reader.onload = (function(aFile) { 
 			return function(evt) { 
-				document.getElementById('dropbox').src = evt.target.result; 
+				var ctx = $('#display').get(0).getContext('2d');
+				var img = new Image();   // Create new img element  
+				img.onload = function(){  
+					$('#display').attr('width', img.width);
+					$('#display').attr('height', img.height);
+					ctx.drawImage(img , 0, 0, img.width, img.height, 0, 0, img.width, img.height);
+				};  
+				img.src = evt.target.result; //'default.png'; // Set source path  	
+				//document.getElementById('dropbox').src = evt.target.result; 
 			} 
 		})(file); 
 
